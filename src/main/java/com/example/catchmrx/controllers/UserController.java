213e75870c7dd0c2ec4cfb9b07f77e58c7a013c3
samarth3 <samarth3@illinois.edu>
2020-05-28 22:42:25 -0500

    Change UI to make websockets work for players

--- src/main/java/com/example/catchmrx/controllers/UserController.java
+++ src/main/java/com/example/catchmrx/controllers/UserController.java
@@ -12,6 +12,6 @@ public class UserController {
     @MessageMapping("/user")
     @SendTo("/topic/user")
     public UserResponse getUser(User user) {
-        return new UserResponse("Hi " + user.getName());
+        return new UserResponse(user.getName() + " " + user.getHostInfo());
     }
 }
--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,25 +1,25 @@
 var stompClient = null;
 
-function setConnected(connected) {
-    $("#connect").prop("disabled", connected);
-    $("#disconnect").prop("disabled", !connected);
-    if (connected) {
-        $("#conversation").show();
-    }
-    else {
-        $("#conversation").hide();
-    }
-    $("#userinfo").html("");
-}
+// function setConnected(connected) {
+//     // $("#connect").prop("disabled", connected);
+//     // $("#disconnect").prop("disabled", !connected);
+//     if (connected) {
+//         $("#conversation").show();
+//     }
+//     else {
+//         $("#conversation").hide();
+//     }
+//     $("#userinfo").html("");
+// }
 
 function connect() {
     var socket = new SockJS('/websocket-example');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
-        setConnected(true);
+        // setConnected(true);
         console.log('Connected: ' + frame);
         stompClient.subscribe('/topic/user', function (greeting) {
-            showGreeting(JSON.parse(greeting.body).content);
+            showMessage(JSON.parse(greeting.body).content);
         });
     });
 }
@@ -28,15 +28,15 @@ function disconnect() {
     if (stompClient !== null) {
         stompClient.disconnect();
     }
-    setConnected(false);
+    // setConnected(false);
     console.log("Disconnected");
 }
 
 function sendName() {
-    stompClient.send("/app/user", {}, JSON.stringify({'name': $("#name").val()}));
+    stompClient.send("/app/user", {}, JSON.stringify({'name': $("#name").val(), 'hostInfo':document.location.host}));
 }
 
-function showGreeting(message) {
+function showMessage(message) {
     $("#userinfo").append("<tr><td>" + message + "</td></tr>");
 }
 
@@ -44,11 +44,8 @@ $(function () {
     $("form").on('submit', function (e) {
         e.preventDefault();
     });
-    $( "#connect" ).click(function() { connect(); });
-    $( "#disconnect" ).click(function() { disconnect(); });
+    // $( "#connect" ).click(function() { connect(); });
+    // $( "#disconnect" ).click(function() { disconnect(); });
     $( "#send" ).click(function() { sendName(); });
-});
-
-$('document').ready(function() {
     connect();
 });
